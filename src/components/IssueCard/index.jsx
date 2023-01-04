3014a9982897a5c0b171925426c1118dcd6b8f5c
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-04 22:50:00 +0900

    feat: 카드 클릭 시 수정 모달 띄우기

--- src/components/IssueCard/index.jsx
+++ src/components/IssueCard/index.jsx
@@ -1,27 +1,34 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
-import { managerList } from '../../temp';
+import IssueModal from '../IssueModal';
 
 export default function IssueCard({ ...props }) {
-  const { title, manager, lastDate, onEdit, onMoveDetail } = props;
+  const { issue } = props;
+  console.log(issue);
+  const [isEdit, setIsEdit] = useState(false);
 
   return (
-    <IssueCardContainer draggable>
-      <p className="title">{title}</p>
-      <p className="manager">{manager}</p>
-      <p className="last-date">{lastDate}</p>
-    </IssueCardContainer>
+    <>
+      <IssueCardContainer draggable onClick={() => setIsEdit(true)}>
+        <p className="title">{issue.title}</p>
+        <p className="manager">{issue.manager}</p>
+        <p className="last-date">{issue.lastDate}</p>
+      </IssueCardContainer>
+      {isEdit && <IssueModal issue={issue} />}
+    </>
   );
 }
 
-const IssueCardContainer = styled.ul`
+const IssueCardContainer = styled.div`
+  cursor: grab;
   padding: 10px;
   margin: 15px 0;
   border: 1px solid var(--gray-200);
   background-color: var(--white);
   border-radius: 5px;
-  -webkit-box-shadow: 0px 0px 14px 0px rgba(174, 174, 174, 0.79);
-  box-shadow: 0px 0px 14px 0px rgba(174, 174, 174, 0.79);
+  -webkit-box-shadow: 0 0 14px 0 rgba(174, 174, 174, 0.79);
+  box-shadow: 0 0 14px 0 rgba(174, 174, 174, 0.79);
 
   & .title {
     font-size: 18px;
@@ -32,6 +39,7 @@ const IssueCardContainer = styled.ul`
     font-size: 14px;
     margin-bottom: 4px;
   }
+
   & .last-date {
     font-size: 12px;
     font-style: bold;
