0753f38c6e2a91d987bdf50d9fd6c5a2a837de12
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-06 01:27:22 +0900

    feat: click 이벤트 대응하는 커스텀 훅 수정

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -4,6 +4,13 @@ const useInput = (initialValue) => {
   const [inputValue, setInputValue] = useState(initialValue);
   const handler = useCallback(
     (e) => {
+      if (e.type === 'click') {
+        setInputValue({
+          ...inputValue,
+          [e.target.closest('li').dataset.name]: e.target.textContent,
+        });
+        return;
+      }
       setInputValue({ ...inputValue, [e.target.name]: e.currentTarget.value });
     },
     [inputValue]
