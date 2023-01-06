306ab933b590b3f55b7113c05be2fbf28ec3b475
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-01-06 12:31:54 +0900

    feat: enums 추가

--- src/enums/index.js
+++ src/enums/index.js
@@ -4,10 +4,29 @@ export const ISSUE_STATE = {
   COMPLETE: { value: 'complete', label: '완료' },
 };
 
+export const ISSUE_MODAL_TITLE = {
+  ADD: '등록',
+  EDIT: '수정',
+};
+
+export const ISSUE_FORM_NAME = {
+  TITLE: 'title',
+  MANAGER: 'manager',
+  DESCRIPTION: 'description',
+  LAST_DATE: 'lastDate',
+};
+
 export const ISSUE_FORM_LABEL = {
   TITLE: '제목',
   MANAGER: '담당자',
-  CONTENT: '내용',
+  DESCRIPTION: '내용',
   STATE: '상태',
-  DUE_DATE: '마감일',
+  LAST_DATE: '마감일',
+};
+
+export const ISSUE_FORM_PLACEHOLDER = {
+  TITLE: '제목을 입력해주세요',
+  MANAGER: '담당자를 선택해주세요',
+  DESCRIPTION: '내용을 입력해주세요',
+  LAST_DATE: '마감일을 선택해주세요',
 };
